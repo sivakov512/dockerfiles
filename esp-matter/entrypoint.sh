64badfc95c7200cc903ae15fbde6d3824630ca23
Nikita Sivakov <sivakov512@icloud.com>
2026-02-28 00:56:58 +0100

    Minimize esp-matter image size

--- esp-matter/entrypoint.sh
+++ esp-matter/entrypoint.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-if [ -n "${IDF_GIT_SAFE_DIR+x}" ]
-then
-	echo "Adding following directories into git's safe.directory"
-	echo "$IDF_GIT_SAFE_DIR" | tr ':' '\n' | while read -r dir
-	do
-		git config --global --add safe.directory "$dir"
-		echo "  $dir"
-	done
-fi
-
-. $IDF_PATH/export.sh
-. $ESP_MATTER_CLONE_PATH/export.sh
+. $IDF_TOOLS_PATH/esp-idf/export.sh
+. $IDF_TOOLS_PATH/esp-matter/export.sh
 
 exec "$@"
